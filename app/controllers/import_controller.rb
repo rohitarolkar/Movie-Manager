6411259e14968f044006b2453867d094ca3dd6c6
iconoclast <iconoclast@iconoclast.(none)>
2011-12-08 16:10:36 +0530

    Innitial Commit

---
+++ app/controllers/import_controller.rb
@@ -0,0 +1,2 @@
+class ImportController < ApplicationController
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppLoki::Application.config.secret_token = '1e99731bb2a4d4149932877b46114ea0bcab842fee92aeaac782f505f85292dc62360c49bd5b6deeb3813e12d646faf6aa7bf26e5648ee1f526e14ecb839d5d3'
